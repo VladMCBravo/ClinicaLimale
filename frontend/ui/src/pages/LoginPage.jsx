55f32803bf7bfb020fc6a60e16494913500a00a6
Vlad Bravo <vladmcbravo@gmail.com>
2025-08-19 19:24:10 -0300

    Tela Login arrumada

--- frontend/ui/src/pages/LoginPage.jsx
+++ frontend/ui/src/pages/LoginPage.jsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
     <Container component="main" maxWidth="xs">
       <Paper elevation={3} sx={{ mt: 8, p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <Typography component="h1" variant="h5">
-        Login TESTE 3
+        Login
         </Typography>
         <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
           <TextField
