e24e9133190df3cd0e2afefa6819f94b6ea66cd5
Vlad Bravo <vladmcbravo@gmail.com>
2025-11-08 21:10:18 -0300

    feat(jornadas): Adiciona API de Jornada de Trabalho

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -3,8 +3,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    CustomAuthTokenLoginView,
-    LogoutView,
     CustomUserViewSet,
     EspecialidadeViewSet,
     JornadaTrabalhoViewSet
@@ -19,8 +17,6 @@ router.register(r'jornadas', JornadaTrabalhoViewSet, basename='jornada')
 # As urlpatterns agora incluem as rotas de autenticação
 # e todas as rotas geradas pelo router.
 urlpatterns = [
-    path('login/', CustomAuthTokenLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
     # Esta linha inclui todas as URLs geradas pelo router
     # Ex: /api/usuarios/, /api/usuarios/<id>/, /api/especialidades/, etc.
     path('', include(router.urls)),
