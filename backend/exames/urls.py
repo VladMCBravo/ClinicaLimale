9f25e2d327af0b4df3292d66c1dd56dd7f764ce6
Vlad Bravo <vladmcbravo@gmail.com>
2025-12-02 20:24:49 -0300

    Cria API publica para acesso de resultados

--- backend/exames/urls.py
+++ backend/exames/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UploadExameView
+from .views import UploadExameView, AcessarResultadosView
 
 urlpatterns = [
     path('upload/', UploadExameView.as_view(), name='upload_exame'),
+    # Nova rota para o paciente:
+    path('acessar/', AcessarResultadosView.as_view(), name='acessar_exame'),
 ]
