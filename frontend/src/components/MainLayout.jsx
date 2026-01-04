56ce4e3922653042d5f4b7540fc2ba57d21fb917
Vlad Bravo <vladmcbravo@gmail.com>
2026-01-04 19:44:54 -0300

    Fix: Atualizacao de arquivos obsoletos

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,13 +1,11 @@
 // src/components/MainLayout.jsx - VERSÃO FINAL
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
 import { Box } from '@mui/material';
 
 export default function MainLayout() {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <Navbar />
       <Box 
         component="main" 
         sx={{ 
