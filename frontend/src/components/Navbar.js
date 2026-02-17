45c67c01a19705064027d7e106ee5b8930eb63a7
Vlad Bravo <vladmcbravo@gmail.com>
2026-02-17 14:13:05 -0300

    Fix: Status de enivos de exames Frontend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,5 @@
 // src/components/Navbar.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import {
