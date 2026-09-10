f0f1495cad2ec002a11d710b268a323959a36c80
Vlad Bravo <vladmcbravo@gmail.com>
2026-09-10 15:35:54 -0300

    feat: Nova area COnfiguraçoes

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -5,7 +5,8 @@ import base64
 from rest_framework import viewsets, generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .permissions import IsAdminUser
 from rest_framework.decorators import action
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
